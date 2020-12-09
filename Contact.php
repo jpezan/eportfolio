cd67d4bb2b7afd8a8da90c09d0529a4dc86daaca
Jacqueline Pezan <71908081+jpezan@users.noreply.github.com>
2020-12-08 20:47:12 -0600

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -1,12 +1,29 @@
 <?php
-    $ToEmail = 'jacquelinempezan@lewisu.edu'; 
-    $EmailSubject = 'ePortfolio Contact Form'; 
-    $mailheader = "From: ".$_POST["email"]."\r\n"; 
-    $mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
-    $mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-    $MESSAGE_BODY = "Name: ".$_POST["Name"].""; 
-    $MESSAGE_BODY .= "Email: ".$_POST["email"].""; 
-    $MESSAGE_BODY .= "Message: ".nl2br($_POST["message"]).""; 
-          mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure");
+$to = 'jacquelinempezan@lewisu.edu';
+$subject = 'ePortfolio Email Contact';
 
-        ?>
+$name = $_POST ['name'];
+$email = $_POST ['email'];
+$message = $_POST ['message'];
+
+$body = <<<EMAIL
+From $name
+$message.
+From : $name
+Email : $email
+Topic : $topic
+EMAIL;
+$header = "From: $email";
+
+if (isset($_POST)) {
+    if ($name == '' || $email == '' || $phone == '' || $message = '') {
+        $feedback = 'Please fill out any missing fields.';
+
+    } else {
+
+    mail( $to, $subject, $body, $header);
+    $feedback = 'Thanks for contacting me, I will get back to you within 24 hours.';
+    }
+}
+?>
+<p class="feedback"> <?php echo $feedback ?> </p>
